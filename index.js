577b0ca098079c2dedc2647d9511539b901cf3a7
tebdpogram <84071136+tebdpogram@users.noreply.github.com>
2021-05-12 15:13:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-xss
+document.getElementById("myNavs").style.width = "100%";
+
+alert('hello');
+	
+	
+    $.getJSON("https://api.ipgeolocation.io/ipgeo?apiKey=63da69e357dc4c9db1b846c3d998343d", function(data) {
+        
+        
+	document.getElementById("ip").innerHTML = (data.ip);
+	
+	document.getElementById("country").innerHTML = (data.country_name);
+		
+		var results = (data["time_zone"]["is_dst"]);
+		
+   document.getElementById("ik").innerHTML = (results);
+   
+   if (results == true){
+	document.getElementById("myNav").style.width = "100%";
+	}
+	else{
+
+	document.getElementById("myNav").style.width = "0%";
+	}
+		
+		docalls();
